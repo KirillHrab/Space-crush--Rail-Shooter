9b46032355693d8be7a9a1a6c5c2dc45f453faf3
KR1st <HrabovskyKirill@gmail.com>
2019-09-17 13:17:10 +0300

    Initial commit

---
+++ Assets/DontDestroy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GameObject[] objct = GameObject.FindGameObjectsWithTag("Music");
+        if (objct.Length > 1)
+            Destroy(this.gameObject);
+        //оло
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    //Loop our music 
+    void Awake()
+    {
+        GameObject[] objct = GameObject.FindGameObjectsWithTag("Music");
+        if (objct.Length > 1)
+            Destroy(this.gameObject);
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadFirstScene", 2f);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+}
