400b0ccfb5d5ff8c3180faa611eaf833787b9eb7
KR1st <HrabovskyKirill@gmail.com>
2019-10-13 16:26:54 +0300

    Tuning project

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+
 
 public class MusicPlayer : MonoBehaviour
 {
@@ -14,19 +14,6 @@ public class MusicPlayer : MonoBehaviour
 
         DontDestroyOnLoad(this.gameObject);
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        Invoke("LoadFirstScene", 2f);
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    void LoadFirstScene()
-    {
-        SceneManager.LoadScene(1);
-    }
+    
     
 }
