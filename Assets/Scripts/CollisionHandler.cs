f850696934ccca8be524519081ab899bdf31f6fb
KR1st <HrabovskyKirill@gmail.com>
2019-11-04 16:14:23 +0300

    Добавляем взрыв при столкновениями с препядствиями
    Добавляем взрыв при столкновениями с препядствиями

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] GameObject deathFX;
     private void OnTriggerEnter(Collider other)
     {
         StartDeath();
@@ -13,7 +15,16 @@ public class CollisionHandler : MonoBehaviour
         
         print("Player Death");
         SendMessage("OnPlayerDeath");
+        deathTrigger();
 
     }
-    
+    void deathTrigger()
+    {
+        Invoke("LoadFirstLevel", 2f);
+        deathFX.SetActive(true);
+    }
+    void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
 }
--- Assets/Scripts/SpaceshipController.cs
+++ Assets/Scripts/SpaceshipController.cs
@@ -37,6 +37,7 @@ public class SpaceshipController : MonoBehaviour
         }
        
     }
+    
     void OnPlayerDeath()
     {
         isControlEnabled = false;
