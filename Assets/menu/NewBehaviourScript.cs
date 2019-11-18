1a6d2736a1e5274e47b1d290229ebb670dfc2eec
Ulkidos <36642350+Ulkidos@users.noreply.github.com>
2019-11-18 21:25:45 +0200

    222

---
+++ Assets/menu/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float yAngle;
+
+   // private GameObject cube1, cube2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        yAngle = 50f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, yAngle*Time.deltaTime, 0);
+    }
+}
